0e955764111323dcd242eca1ee73753496aa7177
Wesley <wrls2@discente.ifpe.edu.br>
2024-07-10 22:04:58 -0300

    Update automação.py

--- automação.py
+++ automação.py
@@ -39,29 +39,29 @@ df = pd.read_csv("")
 print(df)
 # passo 4
 
-for linha in tabela.index:
+for linha in df.index:
     # clicar no campo de código
     pyautogui.click(x=653, y=294)
     # pegar da tabela o valor do campo que a gente quer preencher
-    codigo = tabela.loc[linha, "codigo"]
+    codigo = df.loc[linha, "codigo"]
     # preencher o campo
     pyautogui.write(str(codigo))
     # passar para o proximo campo
     pyautogui.press("tab")
     # preencher o campo
-    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.write(str(df.loc[linha, "marca"]))
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.write(str(df.loc[linha, "tipo"]))
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.write(str(df.loc[linha, "categoria"]))
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.write(str(df.loc[linha, "preco_unitario"]))
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.write(str(df.loc[linha, "custo"]))
     pyautogui.press("tab")
-    obs = tabela.loc[linha, "obs"]
+    obs = df.loc[linha, "obs"]
     if not pd.isna(obs):
-        pyautogui.write(str(tabela.loc[linha, "obs"]))
+        pyautogui.write(str(df.loc[linha, "obs"]))
     pyautogui.press("tab")
     pyautogui.press("enter") # cadastra o produto (botao enviar)
     # dar scroll de tudo pra cima
